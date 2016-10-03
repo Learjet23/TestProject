63d4095648b75e9d2625ddd771d7d928b7b77d57
scott.learjet@gmail.com <Learjet23>
2016-10-03 15:50:51 +0100

    -

--- index.php
+++ index.php
@@ -9,29 +9,11 @@
         <h2>Scott Learmonth</h2>
 
         <?php
-        $name = "Simon";
-        if ($name == "Simon") {
-            print "I know you!";
-        }
-        else {
-            print "Who are you?";
-        }
-
-        $numberOfHobbits = 2;
-        switch ($numberOfHobbits) {
-            case 1:
-                echo "1 sad hobbit";
-                break;
-            case 2:
-                echo "2 happy hobbits";
-                break;
-            case 3:
-                echo "3 hobbits are a crowd";
-                break;
-            default:
-                echo "All the hobbits have gone home";
-        }
-
+            $myArray = array("do", "re", "mi"); // declares the array
+            echo $myArray[0]; // outputs "do"
+            $myArray[1] = "la"; // modifies position 1 (re)
+            echo $myArray[1]; // outputs "la"
+            unset($array[2]); // removes the array in position 2
         ?>
     </body>
 </html>
