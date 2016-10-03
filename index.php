115068969b225cee8cfa3fd15441a83104212a4d
scott.learjet@gmail.com <Learjet23>
2016-10-03 15:26:42 +0100

    -

--- index.php
+++ index.php
@@ -16,6 +16,22 @@
         else {
             print "Who are you?";
         }
+
+        $numberOfHobbits = 2;
+        switch ($numberOfHobbits) {
+            case 1:
+                echo "1 sad hobbit";
+                break;
+            case 2:
+                echo "2 happy hobbits";
+                break;
+            case 3:
+                echo "3 hobbits are a crowd";
+                break;
+            default:
+                echo "All the hobbits have gone home";
+        }
+
         ?>
     </body>
 </html>
