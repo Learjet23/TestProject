fb0ebafa88eba3ac2d8fa9a8215889f16bfddfe8
scott.learjet@gmail.com <Kirsty_23>
2016-10-10 14:31:55 +0100

    connect to database

--- connect.php
+++ connect.php
@@ -22,7 +22,7 @@
 
     //create a SQL query as a string
 
-    $sql_query = "SELECT * FROM superheroes WHERE superpower LIKE '%laser%'";
+    $sql_query = "SELECT * FROM marvelMovies WHERE title LIKE '%Captain%'";
 
     //execute the SQL query
 
